c1b608b525903c44694abaa40ab2b0859e6779d6
YukioE <yukiod8@pm.me>
2024-07-21 19:18:11 +0200

    implements Title screen & Game Over screen, small changes

---
+++ FlappyBlob/src/application/GameState.java
@@ -0,0 +1,7 @@
+package application;
+
+public enum GameState {
+	TITLE_SCREEN,
+	RUNNING,
+	GAME_OVER
+}
--- FlappyBlob/src/application/Main.java
+++ FlappyBlob/src/application/Main.java
@@ -8,43 +8,81 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 
 public class Main extends Application {
 
 	private BlobGame game;
 	private Canvas canvas;
 	private GraphicsContext gc;
+	private AnimationTimer timer;
+	private GameState gameState;
+
+	public void renderTitleScreen(GraphicsContext gc) {
+		game = new BlobGame();
+		game.render(gc);
+		gc.setFill(Color.BLACK);
+		gc.setFont(Font.font("arial", FontWeight.BOLD, FontPosture.REGULAR, 45));
+		gc.fillText("FLAPPYBLOB", game.WINDOW_WIDTH / 2, game.WINDOW_HEIGHT / 2 - 100);
+		gc.fillText("press SPACE to play", game.WINDOW_WIDTH / 2, game.WINDOW_HEIGHT / 2 + 70);
+	}
+
+	public void renderGameOverScreen(GraphicsContext gc) {
+		gc.setFill(Color.BLACK);
+		gc.setFont(Font.font("arial", FontWeight.BOLD, FontPosture.REGULAR, 45));
+		gc.fillText("GAME OVER", game.WINDOW_WIDTH / 2, game.WINDOW_HEIGHT / 2 - 100);
+		gc.fillText("Score: " + game.getScore(), game.WINDOW_WIDTH / 2, game.WINDOW_HEIGHT / 2);
+		gc.fillText("press R to\n play again", game.WINDOW_WIDTH / 2, game.WINDOW_HEIGHT / 2 + 70);
+		game = new BlobGame();
+	}
 
 	public void start(Stage primaryStage) {
 		try {
+			gameState = GameState.TITLE_SCREEN;
 			BorderPane root = new BorderPane();
-			game = new BlobGame();
-			canvas = new Canvas(game.getWINDOW_WIDTH(), game.getWINDOW_HEIGHT());
+			canvas = new Canvas(game.WINDOW_WIDTH, game.WINDOW_HEIGHT);
 			gc = canvas.getGraphicsContext2D();
 			root.getChildren().add(canvas);
+			renderTitleScreen(gc);
 
 			// AnimationsTimer
-			new AnimationTimer() {
+			timer = new AnimationTimer() {
+				@Override
 				public void handle(long now) {
 					game.update();
 					game.render(gc);
 					if (game.checkCollision()) {
-
 						// Highscore speichern falls der aktuelle Score höher ist
 						if (game.getHighScore().getCounter() < game.getScore().getCounter()) {
-							game.getScore().saveScore();	
+							game.getScore().saveScore();
 						}
 						this.stop();
+						gameState = GameState.GAME_OVER;
+						renderGameOverScreen(gc);
 					}
 				}
-			}.start();
+			};
 
-			Scene scene = new Scene(root, game.getWINDOW_WIDTH(), game.getWINDOW_HEIGHT());
+			Scene scene = new Scene(root, game.WINDOW_WIDTH, game.WINDOW_HEIGHT);
 			primaryStage.setScene(scene);
 
 			primaryStage.getScene().setOnKeyPressed(e -> {
 				if (e.getCode() == KeyCode.SPACE) {
-					game.jump();
+					if (gameState == GameState.TITLE_SCREEN) {
+						timer.start();
+						gameState = GameState.RUNNING;
+					}
+					if (gameState == GameState.RUNNING) {
+						game.jump();
+					}
+				} else if (e.getCode() == KeyCode.R && gameState == GameState.GAME_OVER) {
+					renderTitleScreen(gc);
+					gameState = GameState.TITLE_SCREEN;
+
 				}
 			});
 
